525b8b3f5bae3201ee47e07a494c84c16a4336d8
Cauã Pereira <cauapereira_@hotmail.com>
2022-11-09 20:17:34 -0300

    Last Updates

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,62 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/Pet';
+
+import { createMenuObject } from '../helpers/createMenuObject';
+
 export const home = (req: Request, res: Response) => {
 
+    let list = Pet.getAll();
 
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        menu: createMenuObject('all'),
+        list: list
+    })
 };
 
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');
 
+
+    res.render('pages/page', {
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        menu: createMenuObject('dog'),
+        list
+    })
 
 };
 
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');
 
+    res.render('pages/page', {
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        menu: createMenuObject('cat'),
+        list
+    })
 
 };
 
 export const fishes = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');
 
+    res.render('pages/page', {
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        menu: createMenuObject('fish'),
+        list: list
+    })
 
 };
